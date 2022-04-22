dee675bfadc887d6c576f23d5cb613f82eb5085a
Lisa Matza <lisamatz98@gmail.com>
2022-04-22 16:15:19 -0500

    administracionHornos

--- src/components/OvenAdministration.js
+++ src/components/OvenAdministration.js
@@ -15,13 +15,14 @@ import toast from "react-hot-toast";
 import OvenAdministrationDelete from "./OvenAdministrationDelete";
 import OvenAdministrationModal from "./OvenAdministrationModal";
 import OvenAdministrationCard from "./OvenAdministrationCard";
+import OvenAdministrationEdit from "./OvenAdministrationEdit";
 
 const OvenAdministration = () => {
-    const [showAddOven, setShowAddOven] = useState(false);
-    const [ovens, setOvens] = useState([]);
+  const [showAddOven, setShowAddOven] = useState(false);
+  const [ovens, setOvens] = useState([]);
 
-    const [showDeleteModal, setShowDeleteModal] = useState(false);
-    const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
 
   return (
     <div>
@@ -36,30 +37,46 @@ const OvenAdministration = () => {
           title="Agregar horno"
           submitText="Agregar"
         />
-        
+
+        <OvenAdministrationEdit
+          show={showEditModal}
+          onHide={() => setShowEditModal(false)}
+          title="Actualizar horno"
+        />
+
+        <OvenAdministrationDelete
+          show={showDeleteModal}
+          onHide={() => setShowDeleteModal(false)}
+          title="Eliminar horno"
+        />
+        <Row>
+          <Col className=" ms-3 fw-bold fs-1 text-start">
+            Administración de Hornos
+          </Col>
+          <Col className=" me-3 col-4 d-flex align-items-center justify-content-end">
+            <button
+              onClick={() => setShowAddOven(true)}
+              type="button"
+              className="btn btn-secondary text-white "
+            >
+              Agregar horno
+            </button>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={3}>
+            <OvenAdministrationCard
+              oven = {ovens}
+              onEditClicked={() => {
+                setShowEditModal(true);
+              }}
+              onDeleteClicked={() => {
+                setShowDeleteModal(true)
+              }}
+            />
+          </Col>
+        </Row>
       </div>
-      <Row>
-        <Col className=" ms-3 fw-bold fs-1 text-start">
-          Administración de Hornos
-        </Col>
-        <Col className=" me-3 col-4 d-flex align-items-center justify-content-end">
-          <button
-            onClick={() => setShowAddOven(true)}
-            type="button"
-            className="btn btn-secondary text-white "
-          >
-            Agregar horno
-          </button>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={3}>
-          <OvenAdministrationCard
-            onEditClicked={() => {}}
-            onDeleteClicked={() => {}}
-          />
-        </Col>
-      </Row>
     </div>
   );
 };
